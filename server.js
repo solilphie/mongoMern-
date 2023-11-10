ac865274fb042d6397290e4d21802d9c99aea20e
linda meddeb <linda.meddeb@neopolis-dev.com>
2023-11-10 16:12:37 +0100

    add application collection

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const mongoose = require("mongoose");
 const config = require("config");
 const cors = require("cors");
 const app = express();
+
 const users = require("./routes/api/users");
 const posts = require("./routes/api/posts");
+const applications = require("./routes/api/applications")
+
 // const auth = require("./routes/api/auth");
 app.use(express.json());
 app.use(cors());
@@ -14,8 +17,11 @@ mongoose
   .connect(mongo_url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected..."))
   .catch((err) => console.log(err));
+
 // app.use("/api/auth", auth);
 app.use("/api/users", users);
 app.use("/api/posts", posts);
+app.use("/api/applications", applications);
+
 const port = process.env.PORT || 5001;
 app.listen(port, () => console.log(`Server running on port ${port}`));
